a96a5ea0277a726e4bdb33d9c3ee0bd0153731c4
shreyareddy262 <shreyareddysangireddy892@gmail.com>
2024-07-31 13:12:14 +0530

    Add files via upload

---
+++ project shreya.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+pip install rake-nltk
+
+
+# In[2]:
+
+
+import nltk
+nltk.download('punkt')
+from rake_nltk import Rake
+rake_nltk_var = Rake()
+text = """ I am a programmer from India, and I am here to guide you 
+with Data Science, Machine Learning, Python, and C++ for free. 
+I hope you will learn a lot in your journey towards Coding, 
+Machine Learning and Artificial Intelligence with me."""
+rake_nltk_var.extract_keywords_from_text(text)
+keyword_extracted = rake_nltk_var.get_ranked_phrases()
+print(keyword_extracted)
+
+
+# In[ ]:
+
+
+
+
